9be7d2db9dc742f80c7bfa1f402aed08c44b6c5e
SMuvva <SMuvva@192.168.222.1>
2016-12-10 08:57:31 -0600

    updating

---
+++ FaceBook_MavenProject/src/test/java/Login/OpenApp.java
@@ -0,0 +1,25 @@
+package Login;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class OpenApp {
+
+	public static void main(String[] args) {
+		
+		FirefoxDriver driver=new FirefoxDriver();
+		
+		Logger log=Logger.getLogger("devpinoyLogger");
+		
+		driver.get("https://www.facebook.com/");
+		
+		log.debug(" Facebook Application is opened");
+		
+		System.out.println(driver.getTitle());
+		
+		log.debug("Title is displayed");
+
+	}
+
+}
---
+++ FaceBook_MavenProject/src/test/java/Login/sampleTestNG.java
@@ -0,0 +1,12 @@
+package Login;
+
+import org.testng.annotations.Test;
+
+public class sampleTestNG {
+	
+	@Test
+	public void add(){
+		System.out.println("This is ADD");
+	}
+
+}
